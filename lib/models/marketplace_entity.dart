6e140ac4753875b52d14e192369a2a03ec611677
Divye <djain94314@gmail.com>
2022-12-29 18:57:04 +0530

    add some models

---
+++ lib/models/marketplace_entity.dart
@@ -0,0 +1,18 @@
+import 'package:anaaj/models/donor_instituition.dart';
+import 'package:anaaj/models/food_item.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'marketplace_entity.freezed.dart';
+part 'marketplace_entity.g.dart';
+
+@Freezed()
+class MarketplaceEntity with _$MarketplaceEntity {
+  const factory MarketplaceEntity({
+    required String id,
+    required DonorInstituition donorInstituition,
+    // required List<FoodItem> foodItem,
+  }) = _MarketplaceEntity;
+
+  factory MarketplaceEntity.fromJson(Map<String, dynamic> json) =>
+      _$MarketplaceEntityFromJson(json);
+}
