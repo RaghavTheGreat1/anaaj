56abfa6e7a72ac5ade990fb75b50c20ad07ca554
Raghav <developer.raghavjoshi@gmail.com>
2023-01-04 18:46:01 +0530

    add some services

--- lib/cloud/cloud_constants.dart
+++
@@ -1,6 +0,0 @@
-const InstitutionIdRem = 'Institution_id';
-const AddressFieldName = 'Institution_address';
-const QuantityFieldName = 'Institution_food_quantity';
-const TimeFieldName = 'time_of_post';
-
-// const textFieldName = 'text';
