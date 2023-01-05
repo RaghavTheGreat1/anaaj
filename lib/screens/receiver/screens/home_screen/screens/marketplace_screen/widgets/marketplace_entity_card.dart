427744ca4ab0b68e1e4a40d2d0846dbe9fb3dbf2
Raghav <developer.raghavjoshi@gmail.com>
2023-01-05 19:02:01 +0530

    add button to call in entity card

--- lib/screens/receiver/screens/home_screen/screens/marketplace_screen/widgets/marketplace_entity_card.dart
+++ lib/screens/receiver/screens/home_screen/screens/marketplace_screen/widgets/marketplace_entity_card.dart
@@ -1,7 +1,7 @@
 import 'package:anaaj/models/marketplace_entity.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MarketplaceEntityCard extends StatelessWidget {
   const MarketplaceEntityCard({
@@ -13,6 +13,7 @@ class MarketplaceEntityCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Padding(
       padding: const EdgeInsets.fromLTRB(20, 50, 20, 10),
       child: GestureDetector(
@@ -22,67 +23,82 @@ class MarketplaceEntityCard extends StatelessWidget {
             extra: marketplaceEntity,
           );
         },
-        child: SizedBox(
-          width: 570,
+        child: Container(
+          height: 300,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(17.0),
+          ),
           child: Card(
-            elevation: 10,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
+            elevation: 2,
+            surfaceTintColor: Colors.white,
             child: Column(
-              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Image.network(
-                  'https://www.deccanherald.com/sites/dh/files/article_images/2019/11/28/midday%20meal-1574890801.jpg',
-                  width: 400,
-                  height: 200,
-                  fit: BoxFit.fill,
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(17.0),
+                  child: Image.network(
+                    'https://www.deccanherald.com/sites/dh/files/article_images/2019/11/28/midday%20meal-1574890801.jpg',
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.fitWidth,
+                  ),
                 ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30),
-                      child: Text(
-                        marketplaceEntity.donorInstituition.name,
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 12.0,
+                    horizontal: 24,
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            marketplaceEntity.donorInstituition.name,
+                            style: theme.textTheme.titleMedium!.copyWith(
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            marketplaceEntity
+                                .donorInstituition.address.streetAddress,
+                            style: theme.textTheme.titleSmall!.copyWith(
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black.withOpacity(0.5),
+                            ),
+                          ),
+                          Text(
+                            "${marketplaceEntity.donorInstituition.address.city.trim()}, ${marketplaceEntity.donorInstituition.address.state}",
+                            style: theme.textTheme.titleSmall!.copyWith(
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black.withOpacity(0.5),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Icon(
-                        Icons.verified_rounded,
-                        size: 18,
-                        color: Colors.green,
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: () async {
+                              final Uri openDialer = Uri(
+                                scheme: 'tel',
+                                path: "+91" +
+                                    marketplaceEntity
+                                        .donorInstituition.phoneNumber
+                                        .toString(),
+                              
+                              );
+                              await launchUrl(openDialer);
+                            },
+                            icon: Icon(Icons.phone_enabled_outlined),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30),
-                      child: Icon(
-                        Icons.timelapse,
-                        size: 18,
-                        color: Colors.green,
-                      ),
-                    ),
-                    Text("Fresh till almost 8:00pm")
-                  ],
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30),
-                      child: Icon(
-                        Icons.location_on,
-                        size: 18,
-                        color: Colors.green,
-                      ),
-                    ),
-                    Text(
-                      marketplaceEntity.donorInstituition.address.streetAddress,
-                    ),
-                  ],
+                    ],
+                  ),
                 )
               ],
             ),
