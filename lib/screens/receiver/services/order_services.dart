a46043a89dee54fc4b606ec5b02588d6e59cefd9
Raghav <developer.raghavjoshi@gmail.com>
2023-01-05 23:00:01 +0530

    add some order services

---
+++ lib/screens/receiver/services/order_services.dart
@@ -0,0 +1,52 @@
+import 'package:anaaj/models/order.dart' as orderModel;
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class OrderServices {
+  String ordersCollectionPath = 'orders';
+
+  Future<void> placeOrder(orderModel.Order order) async {
+    await FirebaseFirestore.instance
+        .collection(ordersCollectionPath)
+        .doc(order.id)
+        .set(order.toJson());
+  }
+
+  Future<orderModel.Order?> fetchOrderById(String id) async {
+    final fetchedOrder = await FirebaseFirestore.instance
+        .collection(ordersCollectionPath)
+        .doc(id)
+        .get();
+    final rawOrder = fetchedOrder.data();
+    if (rawOrder != null) {
+      final parsedOrder = orderModel.Order.fromJson(rawOrder);
+      return parsedOrder;
+    }
+    return null;
+  }
+
+  Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>>
+      fetchOrdersByReceiverId(String id) async {
+    final fetchedOrder = await FirebaseFirestore.instance
+        .collection(ordersCollectionPath)
+        .where(
+          'receiverInstituition.id',
+          isEqualTo: id,
+        )
+        .get();
+
+    return fetchedOrder.docs;
+  }
+
+  Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>>
+      fetchOrdersByDonorId(String id) async {
+    final fetchedOrder = await FirebaseFirestore.instance
+        .collection(ordersCollectionPath)
+        .where(
+          'donorInstituition.id',
+          isEqualTo: id,
+        )
+        .get();
+
+    return fetchedOrder.docs;
+  }
+}
